dd32a3bae47916d82b1e6a1e3a5f80fedbbd4253
Roberto Albino Junior <junior_wash@hotmail.com>
2022-11-13 22:19:58 -0300

    Ajustando timezone

--- src/main/java/com/roberto/pismo/transactions/TransactionsApplication.java
+++ src/main/java/com/roberto/pismo/transactions/TransactionsApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @ComponentScan("com.roberto.pismo.transactions")
 @SpringBootApplication
 @EnableFeignClients
@@ -17,6 +20,11 @@ public class TransactionsApplication {
 	public static void main(String[] args) {
 		log.info("Serviço de transações iniciado");
 		SpringApplication.run(TransactionsApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
 	}
 
 }
